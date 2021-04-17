53ecceac391dc289c9f65e45f984100ff1a14eeb
Brandon James <brandon@brandonsjames.com>
2021-04-16 23:04:06 -0500

    added help, additional cleanup

--- gopro.sh
+++ gopro.sh
@@ -1,6 +1,6 @@
 trap cleanup TERM INT ERR
 cleanup() {
-    curl -s ${mangled_ip}"/gp/gpWebcam/STOP" >/dev/null 2>&1
+    curl -s ${gopro_ip}"/gp/gpWebcam/STOP" >/dev/null 2>&1
     echo "sent STOP to GoPro"
     sudo modprobe -rf v4l2loopback
     echo "Removed video loopback device"
@@ -8,6 +8,23 @@ cleanup() {
     echo "killed ffmpeg"
     exit
  }
+
+if [[ $1 == '-h' ]];then
+	# Display Help
+	echo "Simple script to use a GoPro as a webcam"
+	echo "Depends on v4l2loopback, ffmpeg and tcpdump"
+	echo
+	echo "Syntax: gopro.sh [-c[c]|p]"
+	echo "options:"
+	echo "p     Open a preview (uncropped in VLC)"
+	echo "c     Crop the output to 720p"
+	echo "cc    Crop the output to 480p"
+	echo 
+	echo "CTRL+C to quit"
+	echo
+
+    exit
+fi
 
 # create video loopback device
 sudo modprobe v4l2loopback exclusive_caps=1 card_label='GoPro' video_nr=42
@@ -18,11 +35,11 @@ dev=$(dmesg | grep cdc_ether | grep -o enp[0-9a-z]* | tail -1)
 echo "assuming $dev is our GoPro interface"
 
 ip=$(ip -4 addr show dev ${dev} | grep -Po '(?<=inet )[\d.]+')
-mangled_ip=$(echo ${ip} | awk -F"." '{print $1"."$2"."$3".51"}')
-echo "using $mangled_ip as GoPro IP"
+gopro_ip=$(echo ${ip} | awk -F"." '{print $1"."$2"."$3".51"}')
+echo "using $gopro_ip as GoPro IP"
 
 # send the start command
-response=$(curl -s ${mangled_ip}"/gp/gpWebcam/START")
+response=$(curl -s ${gopro_ip}"/gp/gpWebcam/START")
 if [ $? -ne 0 ]; then
     echo "Error while starting the Webcam mode."
     cleanup
@@ -35,7 +52,7 @@ sleep 5
 # use tcpdump to find the port the GoPro is sending from
 goproport=$(sudo tcpdump -ni $dev -c 1 dst port 8554 2>/dev/null| grep -o '51.[0-9]*' | grep -o '.[0-9]*$' | cut -c2-)
 echo "sending a packet to UDP $goproport so IPtables marks the traffic as established"
-echo -n "gopro" | nc --send-only -u $mangled_ip $goproport -s $ip -p 8554 >/dev/null
+echo -n "gopro" | nc --send-only -u $gopro_ip $goproport -s $ip -p 8554 >/dev/null
 
 echo "Starting stream CTRL + C to disable webcam"
 if [[ $1 == '-cc' ]]; then
